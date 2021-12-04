970e429d44ada8b734b6f1dbd6c5021f9ebe0e58
Ville Halonen <71012693+hzville@users.noreply.github.com>
2021-12-04 22:29:59 +0200

    delete lukuvinkki feature

--- src/db.py
+++ src/db.py
@@ -23,3 +23,21 @@ class db:
     def find_all_vinkit(self) -> List[KirjaVinkki]:
         """Hakee kaikki kirjavinkit tietokannasta, ja palauttaa ne listana."""
         return self.session.query(KirjaVinkki).all()
+
+    def delete_vinkki_with_id(self, id: int):
+        """Poistaa vinkin id perusteella"""
+        if self.query_with_id(id) != False:
+            self.session.delete(self.query_with_id(id))
+            self.session.commit()
+            return True
+        else:
+            return False
+
+    def query_with_id(self, id: int):
+        """Hakee vinkin id perusteella"""
+        query_result = self.session.query(KirjaVinkki).get(id)
+        
+        if query_result != None:
+            return self.session.query(KirjaVinkki).get(id)
+        else:
+            return False
