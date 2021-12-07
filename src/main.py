58321379705a3ecbca78f603f709d7d5fee61e31
eevahanka <eeva.hanka@iki.fi>
2021-12-07 16:09:55 +0200

    random lukuvinkki

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from models import base
 from ui.ui import Ui
 from ui.io import Io
 from db import DataBase
+from random_generator import random_int
+
 def main():
     io = Io()
     database = DataBase("tietokanta", base)
-    ui = Ui(io, database)
+    ui = Ui(io, database, random_int)
     ui.start()
 
 if __name__ == "__main__":
--- src/tests/ui_test.py
+++ src/tests/ui_test.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import Mock
 from ui.ui import Ui
+from models import KirjaVinkki
 
 class Stub_io:
     def __init__(self, inputs = []):
@@ -13,29 +14,45 @@ class Stub_io:
     def write(self, text):
         self.outputs.append(text)
 
-
 class TestUi(unittest.TestCase):
 
     def setUp(self):
         self.io = Stub_io()
         self.db_mock = Mock()
-        self.stub_ui = Ui(self.io, self.db_mock)
+        self.number_generator_mock = Mock()
+        self.stub_ui = Ui(self.io, self.db_mock, self.number_generator_mock)
 
     def test_legal_input_command(self):
         self.stub_ui.process_command(1)
         self.assertEqual(len(self.io.outputs), 0)
 
     def test_add_new_calls_db_add_vinkki_to_db(self):
-        self.io = Stub_io(["2", "Pro Git Book", "Very cool", "4"])
-        self.stub_ui = Ui(self.io, self.db_mock)
+        self.io = Stub_io(["2", "Pro Git Book", "Very cool", "5"])
+        self.stub_ui = Ui(self.io, self.db_mock, self.number_generator_mock)
         self.stub_ui.start()
 
         self.db_mock.add_vinkki_to_db.assert_called()
 
     def test_print_vinkit_calls_db_find_all_vinkit(self):
-        self.io = Stub_io(["1", "4"])
+        self.io = Stub_io(["1", "5"])
         self.db_mock.find_all_vinkit.return_value = []
-        self.stub_ui = Ui(self.io, self.db_mock)
+        self.stub_ui = Ui(self.io, self.db_mock, self.number_generator_mock)
         self.stub_ui.start()
 
         self.db_mock.find_all_vinkit.assert_called()
+
+    def test_random_vinkki(self):
+        self.io = Stub_io(["4", "5"])
+        self.number_generator_mock = Mock()
+        def find_all_vinkit():
+            vinkki1 = KirjaVinkki(otsikko = "Pro Git Book", kommentti = "Very cool")
+            vinkki2 = KirjaVinkki(otsikko = "python-kirja", kommentti=  "i loved ittt")
+            return [vinkki1, vinkki2]
+        def random_int(max_number):
+            return 1
+        self.number_generator_mock.side_effect = random_int
+        self.db_mock.find_all_vinkit.side_effect = find_all_vinkit
+        self.stub_ui = Ui(self.io, self.db_mock, self.number_generator_mock)
+        self.stub_ui.start()
+        self.assertEqual(self.io.outputs[7].otsikko, "python-kirja")
+
